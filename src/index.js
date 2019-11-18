2d0d240892382a0635e46baa1ab4fcc311b30641
Breyanna Vass <breyannavass@gmail.com>
2019-11-18 13:18:40 -0500

    adding more variables

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
+// @ts-check
+//const { add, subtractm divide, multiply } = require('./calculator');
+
+/**
+ * @file index.js is the root file for this example
+ * @author Breyanna Vass
+ */
+
 /**
  * Student Name
  * @type {string}
  */
 const studentName = "John Doe";
+
+/**
+ * Array of grades
+ * @type {Array<number>}
+ */
+const grades = [98, 97.7, 76, 89];
